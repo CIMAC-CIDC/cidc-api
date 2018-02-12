5859f60c981fca5825f243b044420f0f372a4f4e
Lloyd McCarthy <mccarthy@jimmy.harvard.edu>
2018-02-12 09:35:03 -0500

    Added the beginning of unit testing

---
+++ test_ingestion_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Unit test file for the ingestion API
+"""
+
+
+from eve.tests import TestBase, TestMinimal
+from eve.tests.test_settings import MONGO_DBNAME, MONGO_HOST, MONGO_PASSWORD, MONGO_USERNAME
+from ingestion_api import app
+
+
+class TestIngestionApi(TestMinimal):
+    """[summary]
+    """
+
+    def setUp(self, settings_file=None, url_converters=None):
+        """Sets up the EVE application with settings appropriate to testing
+
+        Keyword Arguments:
+            settings_file {[type]} -- [description] (default: {None})
+            url_converters {[type]} -- [description] (default: {None})
+        """
+        self.app = app
+        self.test_client = app.test_client()
+        self.app.config['TESTING'] = True
+        self.app.config['MONGO_HOST'] = 'localhost'
+
+    def test_post(self):
+        """[summary]
+        """
+        message = {"message": "hi"}
+        r, status_code = self.post('test', message)
+        self.assert201(status_code)
